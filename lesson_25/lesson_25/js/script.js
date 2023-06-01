8b9e585068cd148d66f95a7caa7c2830f99f1262
deordiev-dm <93293534+deordiev-dm@users.noreply.github.com>
2023-06-02 01:20:27 +0300

    finished and reversed

--- lesson_25/lesson_25/js/script.js
+++ lesson_25/lesson_25/js/script.js
@@ -1,4 +1,3 @@
-alert("привіт");
 // ! Задача №1
 // Дано в html: три елементи з класом item.
 // При кліку на кожен з елментів додати клас active,
@@ -88,11 +87,11 @@ function parallaxEffect(e) {
 // ефект друкованого тексту
 
 const typedTextElement = document.querySelector(".typing-text");
-const typedText = typedTextElement.textContent.trim();
+// const typedText = typedTextElement.textContent.trim();
 
-// let userText = prompt("Введи своє ім'я");
-// userText = userText.trim();
-// const typedText = userText + ", ти миле сонечко!";
+let userText = prompt("Введи своє ім'я");
+userText = userText.trim();
+const typedText = userText + ", ти миле сонечко!";
 console.log(typedText);
 const typedTextLength = typedText.length;
 
